adc50b99a2d43cc8f0447a6d63317bff5d81fbc4
Esakkiappan Thevar <thevarbalan32@gmail.com>
2024-01-07 21:51:52 +0530

    basic store setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Auth from './Pages/Auth';
+import { AuthContext } from './Store/AuthProvider';
+import { useContext } from 'react';
 
 function App() {
+
+  const auth=useContext(AuthContext)
+
+  console.log("auth ",auth.isLoggedIn
+  )
   return (
     <div >
      
